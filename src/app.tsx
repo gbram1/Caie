cad775dfb365eeb63f30e80f0e60cb2eb0fa91ea
gbram1 <gabrielt.bram@gmail.com>
2025-04-17 12:29:25 -0400

    mui

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
+import { Button } from '@mui/material'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.body)
-root.render(<h2>Hello from React!</h2>)
+root.render(<Button>Test</Button>)
